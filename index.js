15da12635a89486327d3cec2172ddeae234a7de6
Ronit Raj <47297115+Ronit-Raj9@users.noreply.github.com>
2019-07-12 17:30:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
@@ -64,3 +65,4 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
      console.log('Listening to request on port 8000');
 });
+*/
